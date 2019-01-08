2205e319fbfd986cabe519680658c87834911afd
saintur.b <saintur.b@itzone.mn>
2019-01-09 00:17:56 +0800

    udpate

--- src/main/java/mn/saga/vstream/controllers/ContentController.java
+++ src/main/java/mn/saga/vstream/controllers/ContentController.java
@@ -1,6 +1,7 @@
 package mn.saga.vstream.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,29 @@ public class ContentController {
     public String hello() {
         return "Hello World";
     }
+
+    // home page content
+    @GetMapping("home")
+    public void home() {
+        // list of all medias
+        // trending, recommend, marvel, dc, recently added, favorite, continue to watch from profile list
+
+    }
+
+
+    // determine whether tv show or movies
+    @GetMapping("type/{typeId}")
+    public void type(@PathVariable Long typeId) {
+        // list of medias according to type of {type}
+
+    }
+
+    // determine whether action or comedy etc
+    @GetMapping("genre/{genreId}")
+    public void genre(@PathVariable Long genreId) {
+        // list of medias according to type of {type}
+
+    }
+
+
 }
